364999ba9db05c92b19f96c4be1042973afc754f
Nils van Elswijk <n.elswijk@student.tudelft.nl>
2025-07-03 13:30:16 +0200

    Add MSGraphService + basic api GET

--- java-backend/src/main/java/nils/todo/MSGraphController.java
+++ java-backend/src/main/java/nils/todo/MSGraphController.java
@@ -2,18 +2,27 @@ package nils.todo;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/taskApi")
+@RequestMapping("/taskAPI")
 public class MSGraphController {
 
+    private final MSGraphService graphService;
+
+    public MSGraphController(MSGraphService graphService) {
+        this.graphService = graphService;
+    }
+
+    // Return the top 2 tasks from a list
     @GetMapping("/tasks")
-    public List<String> getAllTasks() {
-        // return all task titles
-        return null;
+    public List<String> getTop2Tasks(@RequestParam String name) {
+        // Call MSGraphService
+        String id = graphService.getTaskListID(name);
+        return graphService.getTop2Tasks(id);
     }
 
 }
---
+++ java-backend/src/main/java/nils/todo/MSGraphService.java
@@ -0,0 +1,27 @@
+package nils.todo;
+import java.util.List;
+
+public class MSGraphService {
+    // Add authenticate and get method here
+
+
+    public String getTaskListID(String displayName) {
+        return null;
+    }
+
+    // Get top 2 tasks
+    public List<String> getTop2Tasks(String id) {
+        // Make call to MSGraph
+
+        // Parse titles
+
+        return null;
+    }
+
+    /*
+    Parse JSON received by MSGraph API
+     */
+    public String parseTitle(String json) {
+        return null;
+    }
+}
